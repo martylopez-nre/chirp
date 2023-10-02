358982815514d12d3ad222fa30833aa3a1ac916d
Marty Lopez <martinmlopez@arizona.edu>
2023-10-02 11:53:32 -0700

    Router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Clerk } from "@clerk/nextjs/dist/types/server";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <ClerkProvider {...pageProps}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+
+    </ClerkProvider>
+
   );
 };
 
