4bf17985abe28c2178f863c5939ce3d7b9ac1332
Marty Lopez <martinmlopez@arizona.edu>
2023-10-02 12:04:28 -0700

    removed auth references

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -13,7 +13,7 @@ import { type Session } from "next-auth";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { getServerAuthSession } from "~/server/auth";
+
 import { db } from "~/server/db";
 
 /**
